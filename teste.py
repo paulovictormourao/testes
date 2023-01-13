5ff401d5b9ec650d93c41a740df1a6ac24e1ea0c
paulovictormourao <mouraopaulovictor@gmail.com>
2023-01-13 19:24:21 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,49 +1,109 @@
 import pygame
+from pygame.locals import *
+from sys import exit
+import math
+
+
+def draw_walls(space1, space2, aux1, space3, space4, space5, aux2):
+
+    # Vertical central Rects
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space1, score_h + border, rects_w1, rects_h1))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space1, (sc_height - border - rects_h1), rects_w1, rects_h1))
+
+    # Four "Ls"
+    # Up L
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space2, field_middle_x - 5*border, rects_h1, rects_w1))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space2 + aux1, field_middle_x - 4 * border, rects_w2, rects_w1))
+    # Below L
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space2, field_middle_x + 4*border, rects_h1, rects_w1))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space2 + aux1, field_middle_x + 3 * border, rects_w2, rects_w1))
+
+    # 2 Rects in the side
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space3 - 2.5 * aux2, score_h + 4 * rects_w1, rects_h1, rects_w1))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space3 - 2.5 * aux2, sc_height - 4 * rects_w1 - border, rects_h1, rects_w1))
+
+    # Big side rect
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space4 - 2.5 * aux2, field_middle_x - rects_w1, rects_h1, 2 * rects_w1))
+
+    # " ] " Symbol
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space3 - aux2, field_middle_x - symbol_h / 2, rects_w1, symbol_h))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space5 - aux2, field_middle_x - symbol_h / 2, rects_w1, rects_w1))
+    pygame.draw.rect(screen, yellow, (sc_width / 2 + space5 - aux2, field_middle_x + symbol_h / 2 - rects_w1, rects_w1, rects_w1))
+
 
 pygame.init()
 pygame.font.init()
 
-size = (1000, 750)
-screen = pygame.display.set_mode(size)
+# Colors
+red = (120, 0, 0)
+yellow = (207, 181, 59)
+
+# Screen
+sc_width = 1180
+sc_height = 760
+screen = pygame.display.set_mode((sc_width, sc_height))
+border = 26
+score_h = 120
+screen.fill(red)
 pygame.display.set_caption("Breakout Game")
-screen.fill("#9f4100")
+font = pygame.font.Font("PressStart2P.ttf", 60)
 
-font = pygame.font.Font("PressStart2P.ttf", 30)
+# Dimensions
+rects_h1 = 2.5 * border
+rects_w1 = border
+rects_w2 = border * 0.9
+field_middle_x = (sc_height + score_h) / 2
+symbol_h = 3 * rects_h1
 
+# Score (player 1)
 score_point_player1 = font.render("0", True, (0, 255, 0))
 score_point_player1_rect = score_point_player1.get_rect()
-score_point_player1_rect.center = (350, 30)
-
+score_point_player1_rect.center = (sc_width/4, 50)
+# Score (player 2)
 score_point_player2 = font.render("0", True, (0, 0, 255))
 score_point_player2_rect = score_point_player2.get_rect()
-score_point_player2_rect.center = (650, 30)
+score_point_player2_rect.center = (3 * sc_width/4, 50)
 
+# Load images
 player1 = pygame.image.load("player1.png")
 player1 = pygame.transform.scale(player1, (50, 50))
 player2 = pygame.image.load("player2.png")
 player2 = pygame.transform.scale(player2, (50, 50))
 player2 = pygame.transform.rotate(player2, 180)
+
+xp1 = 50
+yp1 = field_middle_x - 25
+xp2 = sc_width - 100
+yp2 = field_middle_x - 25
+radius = 25*(2**0.5)
+ang = 135
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    pygame.draw.polygon(screen, "#c29f2e",
-                        [(110, 300), (160, 300), (160, 500), (110, 500), (110, 480), (140, 480), (140, 320),
-                         (110, 320)])
-    pygame.draw.polygon(screen, "#c29f2e",
-                        [(890, 300), (840, 300), (840, 500), (890, 500), (890, 480), (860, 480), (860, 320),
-                         (890, 320)])
-
-    pygame.draw.rect(screen, "#c29f2e", (480, 155, 40, 100))
-    pygame.draw.rect(screen, "#c29f2e", (480, 545, 40, 100))
-    pygame.draw.rect(screen, "#c29f2e", (255, 380, 100, 40))
-    pygame.draw.rect(screen, "#c29f2e", (645, 380, 100, 40))
-
-    pygame.draw.rect(screen, "#c29f2e", (0, 50, 1000, 700), 25)
-    screen.blit(player1, (50, 380))
-    screen.blit(player2, (910, 380))
+    # controls
+    screen.blit(player1, (xp1, yp1))
+    screen.blit(player2, (xp2, yp2))
     screen.blit(score_point_player1, score_point_player1_rect)
     screen.blit(score_point_player2, score_point_player2_rect)
+
+    # Draw
+    # Border horizontal
+    pygame.draw.rect(screen, yellow, (0, score_h, sc_width, border))
+    pygame.draw.rect(screen, yellow, (0, sc_height - border, sc_width, border))
+    # Border vertical
+    pygame.draw.rect(screen, yellow, (0, score_h, border, sc_height))
+    pygame.draw.rect(screen, yellow, (sc_width - border, score_h, border, sc_height))
+
+    # Left
+    draw_walls(-border, -4*border - rects_h1, 0, -15*border, -13*border, -16*border, 0)
+
+    # Right
+    draw_walls(0, 4*border, rects_h1 - rects_w2 + 1, 15 * border, 13 * border, 16 * border, rects_w1)
+
     pygame.display.flip()
+    pygame.display.update()
+    screen.fill(red)
